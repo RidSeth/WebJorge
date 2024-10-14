ba7ac5c592dd7e3b0b604fd1f526ec92b208bbd9
RidSeth <leonardovaldes3@outlook.com>
2024-10-13 23:09:03 -0300

    cambio de texto

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@
 
     $(function () {
         element.typed({
-            strings: ["Web Designer.", "Graphic Designer."],
+            strings: ["Salud mental", "Sesión Psicológica"],
             typeSpeed: 100,
             loop: true,
             autoplay: true,
